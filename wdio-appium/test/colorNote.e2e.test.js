4df46d828d3b383f1db3fe5f19373c807302f402
Parv Khanna <parv.khanna@byjus.com>
2022-12-19 15:53:44 +0530

    Slack notifier testing

--- wdio-appium/test/colorNote.e2e.test.js
+++ wdio-appium/test/colorNote.e2e.test.js
@@ -2,12 +2,17 @@ import AddressPage from "../pages/addressPage";
 import { shoppinglistData } from "../test-data/data";
 import Gestures, { gestures } from "../utils/gestures";
 import { readDataFromCsv, deleteFolder } from '../utils/function'
+import jira from "../config/jira";
+import emailReporter from '../utils/index'
 
 describe('Test case for e2e flow', async function() {
     this.retries(1)
     beforeEach("To restore the app's homepage", async () => {
         await driver.startActivity("com.socialnmobile.dictapps.notepad.color.note", "com.socialnmobile.colornote.activity.Main")
     })
+    afterEach("To raise a bug in Jira", async() => {
+        // await jira.raiseIssue();
+    });
     it('To change the background color of note', async () => {
         
         await AddressPage.skipTutorial()
@@ -44,8 +49,9 @@ describe('Test case for e2e flow', async function() {
         await AddressPage.menuOptions.click();
         await AddressPage.btnsettings.click()
         await driver.pause(5000);
-        const element = await AddressPage.btnAbout;
-        await driver.touchScroll(10, 100, element);
+        //const element = await AddressPage.btnAbout;
+        await $('android = new UiScrollable(new UiSelector().scrollable(true).scrollToEnd(1,5)');
+        await AddressPage.btnAbout.click();
         await driver.pause(8000);
     })
 
